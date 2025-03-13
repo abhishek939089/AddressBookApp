67bbff42fe4f36095a8ac9c8362668cf6e8ba802
Abhishek <ak1824871@gmail.com>
2025-03-13 08:56:36 +0530

    Added: data validation

--- src/main/java/com/example/addressBook/dto/ContactDTO.java
+++ src/main/java/com/example/addressBook/dto/ContactDTO.java
@@ -1,5 +1,7 @@
 package com.example.addressBook.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
@@ -9,7 +11,11 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 public class ContactDTO {
     private Long id;
+
+    @NotBlank(message = "Name is required and cannot be empty")
+    @Pattern(regexp = "^[A-Za-z\\s]{3,50}$", message = "Name must contain only letters and spaces, and be 3 to 50 characters long")
     private String name;
+
     private String email;
     private String phone;
 }
